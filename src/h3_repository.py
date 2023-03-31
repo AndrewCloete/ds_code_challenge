729322ee316aa9a7b48dea7ddadcd0a64fd61455
Andrew Cloete <andrew@x1>
2023-03-31 18:41:24 +0200

    Adds first-pass S3 query event stream JSON parser.

--- src/h3_repository.py
+++ src/h3_repository.py
@@ -5,8 +5,17 @@ References:
 - https://botocore.amazonaws.com/v1/documentation/api/latest/reference/eventstream.html
 """
 
+import json
+
 import boto3
 from secrets_provider import SecretsProvider
+from typing import TypedDict, Optional, List
+
+
+class ReadStreamResponse(TypedDict):
+    data: bytes
+    details: list
+    end_event_received: bool
 
 class H3Repository:
     def __init__(self, region: str, bucket_name: str, secrets_provider: SecretsProvider):
@@ -19,36 +28,42 @@ class H3Repository:
         self.bucket_name = bucket_name
         self.s3 = session.client('s3')
 
-    def cacheFileInTmp(self, key: str):
-        with open(f"/tmp/{key}", 'wb') as f:
-            self.s3.download_fileobj(self.bucket_name, key, f)
-    
-    def queryResolution(self, key: str, resolution: int):
+    def query_features(self, key: str, resolution: int = None):
+
+        def build_expression(resolution: int = None):
+            where_clause = "" if resolution is None else f"WHERE feature.properties.resolution = {resolution}"
+            return f"""SELECT feature FROM S3Object[*].features[*] as feature {where_clause}"""
+            
+        def read_query_event_stream(event_stream) -> ReadStreamResponse:
+            data = b''
+            details = []
+            end_event_received = False
+            for event in event_stream:
+                if 'Records' in event:
+                    data += event['Records']['Payload']
+                # if 'Progress' in event:
+                #     details.append(event['Progress']['Details'])
+                if 'End' in event:
+                    end_event_received = True
+            
+            return {"data": data, "details":details, "end_event_received": end_event_received}
+            
+        def parse_query_response(response) -> Optional[list]:
+            if not response["end_event_received"]:
+                return None 
+            
+            return  [json.loads(line)["feature"] for line in response["data"].split()]
 
         response = self.s3.select_object_content(
             Bucket=self.bucket_name,
             Key=key,
-            Expression=f"SELECT * FROM S3Object[*].features[*] as features  WHERE features.properties.resolution = {resolution}",
+            Expression=build_expression(resolution),
             ExpressionType='SQL',
             InputSerialization={'JSON': { 'Type': 'DOCUMENT' }},
-            OutputSerialization={'JSON': {}}
+            OutputSerialization={'JSON': { }}
             )
 
-
-        event_stream = response['Payload']
-        end_event_received = False
-        # Iterate over events in the event stream as they come
-        for event in event_stream:
-            # If we received a records event, write the data to a file
-            if 'Records' in event:
-                data = event['Records']['Payload']
-                print(data)
-            # If we received a progress event, print the details
-            elif 'Progress' in event:
-                print(event['Progress']['Details'])
-            # End event indicates that the request finished successfully
-            elif 'End' in event:
-                print('Result is complete')
-                end_event_received = True
-        if not end_event_received:
-            raise Exception("End event not received, request incomplete.")
+        streamResult = read_query_event_stream(response['Payload'])
+        return parse_query_response(streamResult)
+        
+        
--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
-"""
-References:
-- https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/select_object_content.html
-- https://docs.aws.amazon.com/AmazonS3/latest/API/API_SelectObjectContent.html#API_SelectObjectContent_RequestSyntax
-- https://botocore.amazonaws.com/v1/documentation/api/latest/reference/eventstream.html
-"""
-
+import json
 from secrets_provider import SecretsProvider
 from h3_repository import H3Repository
 
 REGION = 'af-south-1'
 BUCKET_NAME = 'cct-ds-code-challenge-input-data'
-KEY = 'city-hex-polygons-8-10.geojson'
+KEY_RES_8_to_10 = 'city-hex-polygons-8-10.geojson'
+KEY_RES_8 = 'city-hex-polygons-8.geojson'
 
 
 h3_repo = H3Repository(REGION, BUCKET_NAME, SecretsProvider())
 
-h3_repo.queryResolution(KEY, 8)
+from_all = h3_repo.query_features(KEY_RES_8_to_10, 8)
 
+
+subset = h3_repo.query_features(KEY_RES_8)
+print (json.dumps(subset, indent=2))
+
+print(len(from_all))
+print(len(subset))
 
